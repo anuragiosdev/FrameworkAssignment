4554de887f8243df7eb6435fb72571f7abefd291
Anurag Sharma <anurag.sharma.iosdev@gmail.com>
2022-11-17 21:33:24 +0530

    [WeatherFramework] Added weather framework: initial commit

---
+++ AssignmentWeatherMainApp/WeatherFramework/WeatherFramework.h
@@ -0,0 +1,18 @@
+//
+//  WeatherFramework.h
+//  WeatherFramework
+//
+//  Created by Anurag Sharma on 17/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherFramework.
+FOUNDATION_EXPORT double WeatherFrameworkVersionNumber;
+
+//! Project version string for WeatherFramework.
+FOUNDATION_EXPORT const unsigned char WeatherFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherFramework/PublicHeader.h>
+
+
