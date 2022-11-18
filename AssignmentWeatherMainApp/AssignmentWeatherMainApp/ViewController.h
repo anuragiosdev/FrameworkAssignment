4a08417963a7c9ff41afc1ddf43cb0a5ba2772f7
Anurag Sharma <anurag.sharma.iosdev@gmail.com>
2022-11-18 12:18:05 +0530

    [Framework] Update framework for header file access

--- AssignmentWeatherMainApp/AssignmentWeatherMainApp/ViewController.h
+++ AssignmentWeatherMainApp/AssignmentWeatherMainApp/ViewController.h
@@ -6,6 +6,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <WeatherFramework/WeatherData.h>
 
 @interface ViewController : UIViewController
 
--- AssignmentWeatherMainApp/WeatherFramework/WeatherFramework.h
+++ AssignmentWeatherMainApp/WeatherFramework/WeatherFramework.h
@@ -12,7 +12,9 @@ FOUNDATION_EXPORT double WeatherFrameworkVersionNumber;
 
 //! Project version string for WeatherFramework.
 FOUNDATION_EXPORT const unsigned char WeatherFrameworkVersionString[];
-
+#import <WeatherFramework/WeatherService.h>
+#import <WeatherFramework/WeatherProtocol.h>
+#import <WeatherFramework/WeatherDetail.h>
 // In this header, you should import all the public headers of your framework using statements like #import <WeatherFramework/PublicHeader.h>
 
 
